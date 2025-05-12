f61b2015dd2185464b15edeb6fe38c0bba176e97
arad-5 <arad5@outlook.com>
2025-05-11 17:29:34 -0700

    next.js image config

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,18 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
         remotePatterns: [
-            // لوکال هاست برای توسعه
             {
                 protocol: 'http',
                 hostname: 'localhost',
                 port: '5000',
                 pathname: '/uploads/**',
             },
-            // دامنه پروداکشن (مثلاً yourdomain.com)
             {
                 protocol: 'https',
-                hostname: 'api.amjadec.com', // ← دامنه واقعی خودتو بذار
+                hostname: 'api.amjadec.com',
                 pathname: '/uploads/**',
             },
         ],
